61b742418a035ff03fc0fdfe46bc65222a04ae07
jennafrancis <jef399@nyu.edu>
2017-07-17 14:24:53 -0400

    change Beers component to fit JSON objects

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Beers = ({ beers }) => {
   const renderBeers = beers.map(beer =>
     <div className="show-beer" key={beer.id}>
-      <h3 className="beer-name">{beer.name}, {beer.brewery}</h3>
+      <h3 className="beer-name">{beer.name}, {beer.brewery.name}</h3>
       <p><img src={beer.image_url} alt={beer.name} /></p>
       <p>{beer.style}</p>
       <p>IBU: {beer.ibu} | ABV: {beer.abv}% </p>
