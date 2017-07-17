0f9b09205fd2ec2f6870f29410ba8f6ca9e86cef
jennafrancis <jef399@nyu.edu>
2017-07-17 10:11:24 -0400

    edit css for show_beers formatting

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Beers = ({ beers }) => {
   const renderBeers = beers.map(beer =>
-    <div key={beer.id}>
-      <h3>{beer.name}, {beer.brewery}</h3>
+    <div className="show-beer" key={beer.id}>
+      <h3 className="beer-name">{beer.name}, {beer.brewery}</h3>
       <p><img src={beer.image_url} alt={beer.name} /></p>
       <p>{beer.style}</p>
       <p>IBU: {beer.ibu} | ABV: {beer.abv}% </p>
