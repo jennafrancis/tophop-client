4479745f5c4b5110bef9485d8f5e058ce885620f
jennafrancis <jef399@nyu.edu>
2017-07-18 15:46:23 -0400

    set actions for user authentication

--- src/redux/modules/Auth/actions.js
+++ src/redux/modules/Auth/actions.js
@@ -1,6 +1,5 @@
 import 'isomorphic-fetch';
-
-/* action creators - only synchronous functions */
+import { reset, SubmissionError } from 'reduc-form'
 
 export const authenticationRequest = () => {
   return {
@@ -8,11 +7,34 @@ export const authenticationRequest = () => {
   }
 }
 
-/* async actions - plug into rails api */
+export const setCurrentUser = user => {
+  return {
+    type: 'AUTHENTICATION_SUCCESS',
+    user
+  }
+}
 
-export const signup = (user, router) => {
+export const signup = (userDetails, router) => {
   return dispatch => {
     dispatch(authenticationRequest())
-    return
+    return fetch('/users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ user: userDetails })
+    })
+    .then(response => respons.json())
+    .then(body => {
+      const.slug = body.user.email.split("@")[0]
+      localStorage.setItem('e.tophop.token', body.token;
+      dispatch(setCurrentUser(body.user));
+      dispatch(reset('signup'));
+      router.history.replace(`/users/${slug}`)
+    })
+    .catch(err => {
+      throw new SubmissionError(err)
+    })
   }
 }
--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { signup } from 
 
 class Signup extends Component {
   render() {
