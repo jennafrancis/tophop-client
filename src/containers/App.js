58dc07f2af9ee2d8c41370f7c2e66896f94820bf
jennafrancis <jef399@nyu.edu>
2017-07-18 13:59:26 -0400

    move components to own files

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,15 +5,16 @@ import {
   Switch,
   NavLink
 } from 'react-router-dom';
-import AddBeer from './components/AddBeer';
-import Beers from './components/Beers';
-import Navbar from './components/Navbar';
-import BeerService from './services/BeerService';
-import './App.css';
 
-const Signup = () => <div>Signup</div>
-const Home = () => <div>Home</div>
-const NotFound = () => <div>NotFound</div>
+import Home from '../views/Home';
+import Signup from '../views/Signup';
+import NotFound from '../views/NotFound';
+
+import AddBeer from '../components/AddBeer';
+import Beers from '../components/Beers';
+import Navbar from '../components/Navbar';
+import BeerService from '../services/BeerService';
+import '../App.css';
 
 class App extends Component {
   constructor() {
@@ -43,15 +44,16 @@ class App extends Component {
             <NavLink className="navlink" to="/">Home</NavLink>
             <NavLink className="navlink" to="/signup">Signup</NavLink>
           </div>
+          <div className="title">
+            <h1>TopHop</h1>
+            <p>Something clever about craft beers</p>
+          </div>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/signup" component={Signup} />
             <Route component={NotFound} />
           </Switch>
-          <div className="title">
-            <h1>TopHop</h1>
-            <p>Something clever about craft beers</p>
-          </div>
+
           <div className="main">
             <Beers beers={this.state.beers}/>
           </div>
---
+++ src/views/Signup/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+
+const Signup = () =>
+<form className="signup_form"><label>"I'm a form!"</label></form>
+
+export default Signup;
