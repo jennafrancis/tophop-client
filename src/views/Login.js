e55ca2d00ab1537c2ac40ed4fbbcd0a53684c87d
jennafrancis <jef399@nyu.edu>
2017-07-19 01:35:57 -0400

    loginform component

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,33 +1,18 @@
 import React from 'react';
 // import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form'
-const  { DOM: { input } } = React
 
-const LoginForm = (props) => {
-  const { handleSubmit, pristine, reset, submitting } = props
-    return (
-      <div className="login">
-        <h3>Welcome back!</h3>
-        <div>
-          <label>Email</label>
-          <div>
-            <Field name="email" component={input} type="email" placeholder="Email"/>
-          </div>
-        </div>
-        <div>
-          <label>Password</label>
-          <Field name="password" component={password =>
-            <div>
-              <input type="password" {...password} placeholder="Password"/>
-              {password.touched && password.error && <span>{password.error}</span>}
-            </div>
-          }/>
-        </div>
-      </div>
-    )
-  }
+const doSubmit = values => ajax.post('api/login', { data: values })
+
+const LoginForm = (props) => (
+  <form onSubmit={props.handleSubmit(doSubmit)}>
+    <Field name="email" component="input" type="text" placeholder="Email" />
+    <Field name="password" component="input" type="password" placeholder="Password" />
+    <button type="submit">Log in</button>
+  </form>
+)
 
 
 export default reduxForm({
-  form: 'login'
+  form: 'loginForm'
 })(LoginForm);
