7fcbaa5e10a5b6614a1d941ea99f618545e4b0e2
jennafrancis <jef399@nyu.edu>
2017-07-23 15:07:29 -0400

    fix bug on love it button, add hate it button

--- src/components/beers/BeerList.js
+++ src/components/beers/BeerList.js
@@ -5,7 +5,7 @@ import BeerLove from './BeerLove'
 class BeerList extends Component {
   render() {
     const renderBeers = this.props.beers.map(beer =>
-      <BeerLove beer={beer} count='5' />
+      <BeerLove beer={beer} count={5} />
     );
 
     return (
--- src/components/beers/BeerLove.js
+++ src/components/beers/BeerLove.js
@@ -10,9 +10,17 @@ class BeerLove extends Component {
     }
   }
 
-  onClick = () => {
+  onLove = () => {
     let count = this.state.counter
     count += 1
+    this.setState({
+      counter: count
+    })
+  }
+
+  onHate = () => {
+    let count = this.state.counter
+    count -= 1
     this.setState({
       counter: count
     })
@@ -23,8 +31,9 @@ class BeerLove extends Component {
       <div className="beer-list" key={this.props.beer.id}>
         <Link to={`/beers/${this.props.beer.id}`} className="beer-basic"><h4 >{this.props.beer.name}, {this.props.beer.brewery}</h4></Link>
         <p><img className="home-img" src={this.props.beer.image_url} alt={this.props.beer.name} /></p>
-        <button onClick={this.onClick}>Love it!</button>
-        <p>How much do you love it? {this.state.counter}</p>
+        <button onClick={this.onLove}>Love it!</button>
+        <button onClick={this.onHate}>Hate it!</button>
+        <p>Current love: {this.state.counter}</p>
       </div>
     )
   }
