7c6a16021b451a4c42bf23663009a77a75e6b842
jennafrancis <jef399@nyu.edu>
2017-08-05 17:31:04 -0400

    signup actions set

---
+++ src/redux/actions/signup.js
@@ -0,0 +1,61 @@
+import 'isomorphic-fetch';
+import { reset, SubmissionError } from 'redux-form'
+const API_URL = process.env.REACT_APP_API_URL;
+
+function authenticationRequest(creds) {
+  return {
+    type: 'AUTHENTICATION_REQUEST',
+    isAuthenticating: true,
+    isAuthenticated: false,
+    creds
+  }
+}
+
+function setCurrentUser(user) {
+  return {
+    type: 'AUTHENTICATION_SUCCESS',
+    isAuthenticating: false,
+    isAuthenticated: true,
+    currentUser: user
+  }
+}
+
+function signupError(message) {
+  return {
+    type: 'AUTHENTICATION_FAILURE',
+    isAuthenticating: false,
+    isAuthenticated: false,
+    message
+  }
+}
+
+export default function signup(creds, router) {
+  let config = {
+    method: 'POST',
+    body: JSON.stringify({
+      user: creds
+    }),
+    headers: {
+      'Accept' : 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+
+  return dispatch => {
+    dispatch(authenticationRequest(creds))
+    return fetch(`${API_URL}/users`, config)
+    .then(response => response.json())
+    .then(body => {
+      if (body.user.id) {
+        localStorage.setItem('tophop.token', body.token;
+        dispatch(setCurrentUser(body.user));
+        router.history.replace(`/`)
+        // const.slug = body.user.email.split("@")[0] <-- for future implementation
+        // router.history.replace(`/users/${slug}`)
+      } else {
+        dispatch(signupError(body.errr))
+        return Promise.reject(body)
+      }
+    }).catch(err => console.log("Error: ", err))
+  }
+}
